1fc2ab28c01e05eea2c7a74475ed5ee9ec06a1ff
Thomas Dye <thomas@thomasdye.net>
2024-05-19 11:01:57 +0000

    init

---
+++ TitanImport/Program.cs
@@ -0,0 +1,22 @@
+﻿using Blazor.FileReader;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TitanImport
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSingleton(new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);            
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
